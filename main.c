f7852289c521f0c5dfc1384bd61f6669a353a5f7
Ian Shearin <shearini@onid.orst.edu>
2013-02-03 11:21:22 -0800

    Added command-line argument parsing.

--- main.c
+++ main.c
@@ -8,15 +8,66 @@
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 
 
 
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
+	char *in_filename;
+	FILE in_file;
+	char *out_filename;
+	FILE out_file;
+
+	int action;
+
+	int arg;
+
+
+	/* Set default settings. */
+	//in_file = (FILE)NULL;
+	//out_file = (FILE)NULL;
+	action = ACT_LEX;
+
+	/* Parse command-line arguments. */
+	for (arg = 1; arg < argc; arg++)
+	{
+		if ((strcmp(argv[arg], "--help") == 0)
+			|| (strcmp(argv[arg], "-h") == 0))
+		{
+			action = ACT_USAGE;
+		}
+		else if ((strcmp(argv[arg], "--lex") == 0))
+		{
+			action = ACT_LEX;
+		}
+		else
+		{
+			fprintf(stderr, "Invalid argument: %s\n", argv[arg]);
+			/* Stop parsing command-line. */
+			arg = argc;
+
+			action = ACT_USAGE;
+		}
+	}
+
+	/* Take action. */
+	if (action == ACT_USAGE)
+	{
+		printf(
+			"Usage: compiler [option...] [infile]\n"
+			"\n"
+			"  Options:\n"
+			"\n"
+			"  -h, --help  Display this help text.\n"
+			"      --lex   Run the lexer.\n"
+		);
+	}
+	else if (action == ACT_LEX)
+	{
+		printf("Lexing...\n");
+	}
 
 	return EXIT_SUCCESS;
 }
---
+++ main.h
@@ -0,0 +1,20 @@
+/*
+ *  main.h
+ *
+ *  Ian Shearin
+ *  CS480
+ */
+
+
+
+
+#define ACT_NULL 0x00
+#define ACT_USAGE 0x01
+#define ACT_LEX 0x02
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
