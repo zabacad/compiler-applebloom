6c2d13e8d1c795127c666d6ee49ec92d5bb1cd80
Ian Shearin <shearini@onid.orst.edu>
2013-02-04 16:26:21 -0800

    Completed token set detail function. Now casts to the proper type depending on token class.

--- token.c
+++ token.c
@@ -33,5 +33,21 @@ void token_set_class(token_t token, int class)
 
 void token_set_detail(token_t token, void *detail)
 {
-	token->detail = (union token_detail_t)*(char *)detail;
+	switch (token->class)
+	{
+		case TOKEN_PAREN:
+		case TOKEN_BOOL:
+		case TOKEN_BOOL_OP:
+		case TOKEN_NUM_OP:
+		case TOKEN_REAL_OP:
+		case TOKEN_STR:
+		case TOKEN_STR_OP:
+		case TOKEN_REL:
+			token->detail = (union token_detail_t)*(char *)detail;
+			break;
+		case TOKEN_INT:
+		case TOKEN_REAL:
+			token->detail = (union token_detail_t)*(double *)detail;
+			break;
+	}
 }
