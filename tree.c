9c41f1c41bb7a9f92bd127aeed8d823fc0c3c89c
Ian Shearin <shearini@onid.orst.edu>
2013-02-15 17:08:03 -0800

    Worked on tree.

--- tree.c
+++ tree.c
@@ -9,3 +9,57 @@
 
 
 #include "tree.h"
+
+
+
+
+tree_t *tree_create(void *data, size_t data_size)
+{
+	tree_t *tree;
+	tree_node_t *root;
+
+
+	tree = (tree_t *)malloc(sizeof(tree_t));
+
+	root = tree_node_create(data, data_size);
+
+	tree->root = root;
+
+	return tree;
+}
+
+
+void tree_destroy(tree_t *tree)
+{
+	tree_node_destroy(tree->root);
+	free(tree);
+}
+
+
+tree_node_t *tree_node_create(void *data, size_t data_size)
+{
+	tree_node_t *node;
+
+
+	node = (tree_node_t *)malloc(sizeof(tree_node_t));
+	node->num_children = 0;
+	node->data = malloc(data_size);
+	memcpy(node->data, data, data_size);
+
+	return node;
+}
+
+
+void tree_node_destroy(tree_node_t *node)
+{
+	int child;
+
+
+	for (child = 0; child < node->num_children; child++)
+	{
+		tree_node_destroy(&node->children[child]);
+	}
+
+	free(node->data);
+	free(node);
+}
--- tree.h
+++ tree.h
@@ -4,7 +4,41 @@
  *  Ian Shearin
  *  CS480
  */
-#ifndef BUFFER_H
-#define BUFFER_H
+#ifndef TREE_H
+#define TREE_H
 
-#endif /* BUFFER_H */
+
+
+
+#include "string.h"
+
+
+
+
+typedef struct tree tree_t;
+typedef struct tree_node tree_node_t;
+
+struct tree
+{
+	tree_node_t *root;
+};
+
+struct tree_node
+{
+	void *data;
+	tree_node_t *children;
+	int num_children;
+};
+
+
+
+
+tree_t *tree_create(void *data, size_t data_size);
+void tree_destroy(tree_t *tree);
+tree_node_t *tree_node_create(void *data, size_t data_size);
+void tree_node_destroy(tree_node_t *node);
+
+
+
+
+#endif /* TREE_H */
